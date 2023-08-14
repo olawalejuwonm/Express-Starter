a13ec24c2c3db4140f34b79f9406cc41eb75c497
olawalejuwonm <olawalejuwon@gmail.com>
2023-08-14 14:50:18 +0100

    clear init

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,12 +2,6 @@ import { getModelForClass } from '@typegoose/typegoose';
 import { Profile } from './profileModel';
 import { File } from '../features/file/schema';
 
-// import { Profile } from './models/profileModel';
-// import { Level } from './features/level/model';
-// import { Department } from './features/department/model';
-// import { LeaveType } from './features/leaveType/model';
-// import { Position } from './features/position/model';
-// import { LeaveRequest } from './features/leaveRequest/model';
 
 export const ProfileModel = getModelForClass(Profile);
 export const FileModel = getModelForClass(File);
