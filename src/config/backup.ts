86940a8a570acac4c1644a8ebe6a336d2339b24d
Olawale Micheal Juwon <olawalejuwon@gmail.com>
2024-05-10 15:51:40 +0100

    chore: Add node-mongotools dependency and import backup configuration

---
+++ src/config/backup.ts
@@ -0,0 +1,39 @@
+import os from 'os';
+
+export default async function dumpAndRotate(uri: string, path: string) {
+  // @ts-ignore
+  const { MongoTools, MTOptions, MTCommand } = await import('node-mongotools');
+  var mt = new MongoTools();
+  var mtc = new MTCommand(); // to reuse log methods
+  // mongodump
+  const dumpResult = await mt
+    .mongodump({ uri, path, dropboxToken: process.env.DROPBOX_SECRET_TOKEN})
+    .catch(mtc.logError.bind(mtc));
+
+
+  console.log(mt, mtc, dumpResult);
+  if (dumpResult === undefined) {
+    // error case
+    // process.exit(1);
+    console.log('dumpResult is undefined');
+    return;
+  }
+  mtc.logSuccess(dumpResult);
+
+ // backups rotation
+  const rotationResult = await mt
+    .rotation({ path, rotationWindowsDays: 5, rotationMinCount: 1 })
+    .catch(mtc.logError.bind(mtc));
+  if (rotationResult === undefined) {
+    // error case
+    // process.exit(1);
+    console.log('rotationResult is undefined');
+    return;
+  }
+  mtc.logSuccess(rotationResult);
+}
+
+const uri = process.env.MONGODB_URI;
+const path = `backup/${os.hostname()}`;
+
+dumpAndRotate(uri, path);
